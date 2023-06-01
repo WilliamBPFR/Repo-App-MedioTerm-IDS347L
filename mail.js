a0b3ecaf00a9fe32b9f8289bcd9b3177695a0258
Jean Brugal <jpbrugal@hotmail.com>
2023-06-01 19:44:21 -0400

    Nuevo correo.

--- mail.js
+++ mail.js
@@ -17,7 +17,7 @@ const transporter = nodemailer.createTransport({
 function sendEmail(subject, text) {
   const mailOptions = {
     from: 'correopruebaapp1232@gmail.com',
-    to: 'williamchawillferferreira@gmail.com',
+    to: 'jpbrugal81@gmail.com',
     subject: subject,
     text: text,
   };
@@ -31,5 +31,40 @@ function sendEmail(subject, text) {
   });
 }
 
+const letraCancion = `[Letra de "¿Qué Carajos Quieres Tú Ahora?"]
 
-sendEmail('MORA LA CABRA','ONE PIECE ES EL MEJOR ANIME');
+[Intro]
+Eladio Carrión, ¡sendo cabrón!
+
+[Coro]
+¿Y qué carajo quieres tú ahora? (Wuh)
+Rollie bust down, nigga, ni veo la hora (Clink)
+Yo estoy en la mía to' los días, no me joda'
+El dinero es tiempo, pero yo te cobro un par de hora' (Jeje)
+
+[Verso 1]
+Cuando lavo ropa no chequeo los bolsillo', siempre encuentro par de mile' en la secadora (¡Eh!)
+En el jet pri 'toy contando tanto money, creo que yo necesito una calculadora (¡Moolah!)
+En el jetski a dosciento' yo no fallo, mira cómo los tengo montado' en la ola (Skrrt-skrrt)
+Ella aventurea como Dora La Explora, en el backpack codе y de crema la soda
+To' los Phillie' tienen hash, mira el cherry, belladona
+En Barcelona coronando con el Beny, con el Morad
+Dile que esto es Sauce Boyz, nigga, esto no e' una moda (Nah)
+En el Backwood, wedding cake sin la boda (Damn)
+Tengo moña' del tamaño 'e baby Yoda (Jajaja)
+Tengo sniper como Kodak (Pum, pum, pum, pum)
+Fumo tanto que mi grinder es una batidora
+Ela con el trap es como Kobe con la bola
+
+[Pre-Coro]
+Dime qué carajos quieres tú ahora (¿Qué?)
+¿Qué carajos quieres tú ahora? (¿Qué?)
+Estos cabrone' ponen tantos huevo' que les voy a regalar una incubadora (Hey!)`;
+
+console.log(letraCancion);
+
+sendEmail('ELADIO LA CABRA',letraCancion);
+
+
+
+
