4ae95b300e7add8164f519dd755a1c7ad6b112c9
Jean Brugal <jpbrugal@hotmail.com>
2023-06-01 19:35:05 -0400

    Base de datos creada

---
+++ ejemploDB.js
@@ -0,0 +1,21 @@
+const database = require("./database");
+
+async function example (){
+
+    const Nota = new database({});
+
+    Nota.title = "Juego";
+    Nota.email = "johnny.ventura@gmail.com";
+    Nota.message = "palomo";
+    Nota.fecha = "08/07/2023";
+
+    try {
+        const product = await Nota.save();
+        console.log(product);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+example();
+
